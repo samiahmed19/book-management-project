7972e5150a34f1c05c1837dbfe051ee9f4897f6e
Sami <119399293+samiahmed19@users.noreply.github.com>
2023-10-03 11:58:17 +0530

    Delete router/authRoutes.js

--- router/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/authController');
-const { validateSignUp, validateSignIn } = require('../validator/validation');
-
-router.post('/signup', validateSignUp, authController.signUp);
-router.post('/signin', validateSignIn, authController.signIn);
-
-module.exports = router;
